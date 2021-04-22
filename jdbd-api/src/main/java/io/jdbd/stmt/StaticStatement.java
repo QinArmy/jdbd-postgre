57a685556f1e90d676da89bfee04d49b89512500
zoro <zoro@qinarmy.org>
2021-04-23 05:04:08 +0800

    MySQL module v129,add executeAsFlux method and  ComQueryTask 2.0, refactor DownstreamSink .

--- jdbd-api/src/main/java/io/jdbd/stmt/StaticStatement.java
+++ jdbd-api/src/main/java/io/jdbd/stmt/StaticStatement.java
@@ -27,20 +27,126 @@ public interface StaticStatement extends Statement {
 
 
     /**
-     * Executes the SQL statement in this <code>PreparedStatement</code> object,
-     * which must be an SQL Data Manipulation Language (DML) statement, such as <code>INSERT</code>, <code>UPDATE</code> or
-     * <code>DELETE</code>; or an SQL statement that returns nothing,
-     * such as a DDL statement.
-     *
+     * Executes the given SQL statement(no parameter placeholder) thant can only producer one update result.
+     * for example :
+     * <ul>
+     *     <li>INSERT</li>
+     *     <li>UPDATE</li>
+     *     <li>DELETE</li>
+     *     <li>CREATE TABLE</li>
+     *     <li>CALL Stored procedures that just produce one update result and no out parameter.</li>
+     * </ul>
+     * this method like {@code java.sql.Statement#executeUpdate(String)}
      * <p>
-     * Mono publish {@link JdbdSQLException}, if a database access error occurs;
-     * this method is called on a closed  <code>PreparedStatement</code>
-     * or the SQL statement returns a <code>ReactiveResultSet</code> object
+     *     Below are correct examples:
+     *     <pre>
+     *         <code>
+     *              //correct example 1:
+     *              String sql == "INSERT INTO user(name,age) VALUE('qinarmy',1)";
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *         </code>
+     *         <code>
+     *              //correct example 2:
+     *              String sql == "UPDATE user as u SET u.name = 'qin' WHERE u.id = 1";
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *         </code>
+     *         <code>
+     *              //correct example 3:
+     *              CREATE PROCEDURE single_result_procedure()
+     *              BEGIN
+     *                  UPDATE user as u SET u.name = 'qin' WHERE u.id = 1
+     *              END;
+     *
+     *              String sql == "CALL single_result_procedure()";
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *         </code>
+     *         <code>
+     *              //correct example 4:
+     *              StaticStatement stmt = databaseSession.statement();
+     *              stmt.setExecuteTimeout(10) // you can reuse stmt ,if you don't invoke any setXxx method again.
+     *
+     *              String sql1 == "INSERT INTO user(name,age) VALUE('qinarmy',1)";
+     *              String sql2 == "UPDATE user as u SET u.name = 'qin' WHERE u.id = 1";
+     *              Mono.from(stmt.executeUpdate(sql1))
+     *                  .map(this::handleFirstUpdate) // handleFirstUpdate is a method
+     *                  .then( Mono.from(stmt.executeUpdate(sql2)) ) // you can directly use 'then' method,because executeUpdate method return a deferred publisher.
+     *                  .map(this::handleSecondUpdate) // handleSecondUpdate is a method
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *         </code>
+     *     </pre>
+     *
      * </p>
      *
-     * @return either (1) the row count for SQL Data Manipulation Language (DML) statements
-     * or (2) 0 for SQL statements that return nothing
-     * @see java.sql.PreparedStatement#executeUpdate()
+     * <p>
+     *     <pre>
+     *     Below are error examples:
+     *         <code>
+     *              //error example 1:
+     *              String sql == "INSERT INTO user(name,age) VALUE('qinarmy',?)"; // can't execute sql that contain any parameter placeholder.
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *             //above emit JdbdSQLException
+     *         </code>
+     *         <code>
+     *              //error example 2:
+     *              String sql == "SELECT u.name FROM user as u LIMIT 1"; // can't execute any sql that produce query result.
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *             //above emit SubscribeException
+     *         </code>
+     *         <code>
+     *              //error example 3:
+     *              CREATE PROCEDURE multi_result_procedure()
+     *              BEGIN
+     *                  UPDATE user as u SET u.name = 'qin' WHERE u.id = 1
+     *                  UPDATE user as u SET u.name = 'qin' WHERE u.id = 2
+     *              END;
+     *
+     *              String sql == "CALL multi_result_procedure()"; // can't execute any sql that produce multi-result.
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *              //above emit SubscribeException
+     *         </code>
+     *         <code>
+     *              //error example 4:
+     *              CREATE PROCEDURE single_result_procedure(out rows INT)
+     *              BEGIN
+     *                   SELECT 1 INTO rows;
+     *              END;
+     *
+     *              String sql == "CALL single_result_procedure()"; // can't execute sql that producer out parameter
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *              //above emit SubscribeException
+     *         </code>
+     *         <code>
+     *              //error example 5:
+     *              String sql == "UPDATE user as u SET u.name = 'qin' WHERE u.id = 1;INSERT INTO user(name,age) VALUE('qinarmy',1)";
+     *              // can't execute any sql that is multi-statement
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *             //above emit JdbdSQLException
+     *         </code>
+     *         <code>
+     *              //error example 6:
+     *              String sql == "INSERT INTO user(name,age) VALUE('qinarmy',1) RETURNING * "; // can't execute sql that producer any column.
+     *              Mono.from(stmt.executeUpdate(sql))  // stmt is io.jdbd.stmt.StaticStatement instance.
+     *                  .subscribe() // if no subscribe ,don't communication with database server
+     *             //above emit SubscribeException
+     *         </code>
+     *     </pre>
+     * </p>
+     *
+     * @param sql sql thant can only producer one update result.
+     * @return a deferred publisher that emit at most one element, for example {@code reactor.core.publisher.Mono},
+     * no communication with database server util subscribe.
+     * @throws io.jdbd.SessionCloseException emit when {@link io.jdbd.DatabaseSession} has closed.
+     * @throws SubscribeException            emit when sql produce query result or multi-result.
+     * @throws JdbdSQLException              emit when sql execution occur error.
+     * @throws io.jdbd.JdbdNonSQLException   emit when if occur other error.
      */
     Publisher<ResultStatus> executeUpdate(String sql);
 
