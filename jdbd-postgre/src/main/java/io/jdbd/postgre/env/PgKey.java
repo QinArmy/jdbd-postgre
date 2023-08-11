33f2f341df0a597bc51934127db21660fc9233b0
zoro <zoro@qinarmy.io>
2023-08-11 21:16:05 +0800

    refactor jdbd-postgre v46, postgre properties v1

--- jdbd-postgre/src/main/java/io/jdbd/postgre/env/PgKey.java
+++ jdbd-postgre/src/main/java/io/jdbd/postgre/env/PgKey.java
@@ -3,10 +3,8 @@ package io.jdbd.postgre.env;
 import io.jdbd.lang.Nullable;
 import io.jdbd.postgre.util.PgCollections;
 import io.jdbd.vendor.env.Key;
-import io.jdbd.vendor.env.Redefine;
 import io.jdbd.vendor.task.SslMode;
 
-import java.nio.file.Path;
 import java.util.List;
 
 
@@ -22,160 +20,257 @@ public final class PgKey<T> extends Key<T> {
 
     // ssl group
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<String> SSL_FACTORY = new PgKey<>("sslFactory", String.class, null);
 
-    sslfactory(Class .class),
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<SslMode> SSL_MODE = new PgKey<>("sslMode", SslMode.class, SslMode.DISABLED);
 
-    @Deprecated
-    sslfactoryarg(String .class),
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<String> SSL_CERT = new PgKey<>("sslCert", String.class, null);
 
-    @Redefine
-    sslmode(SslMode .class, "DISABLED"),
+    public static final PgKey<String> SSL_KEY = new PgKey<>("ssl_key", String.class, null);
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
-    sslcert(Path .class),
+    public static final PgKey<String> SSL_ROOT_CERT = new PgKey<>("sslRootCert", String.class, null);
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
-    sslkey(Path .class),
+    public static final PgKey<String> SSL_HOST_NAME_VERIFIER = new PgKey<>("sslHostNameVerifier", String.class, null);
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<String> sslPasswordCallback = new PgKey<>("sslPasswordCallback", String.class, null);
 
-    sslrootcert(String .class),
+    public static final PgKey<String> SSL_PASSWORD = new PgKey<>("sslPassword", String.class, null);
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<String> PROTOCOL_VERSION = new PgKey<>("protocolVersion", String.class, null);
 
-    sslhostnameverifier(Class .class),
+    public static final PgKey<Boolean> ALLOW_ENCODING_CHANGES = new PgKey<>("allowEncodingChanges", Boolean.class, Boolean.FALSE);
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<Boolean> LOG_UNCLOSED_CONNECTIONS = new PgKey<>("logUnclosedConnections", Boolean.class, Boolean.FALSE);
 
-    sslpasswordcallback(Class .class),
+    public static final PgKey<Enums.AutoSave> AUTO_SAVE = new PgKey<>("autoSave", Enums.AutoSave.class, Enums.AutoSave.never);
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<Boolean> cleanupSavePoints = new PgKey<>("cleanupSavePoints", Boolean.class, Boolean.FALSE);
 
-    sslpassword(String .class),
 
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+    public static final PgKey<Boolean> BINARY_TRANSFER = new PgKey<>("binaryTransfer", Boolean.class, Boolean.TRUE);
 
-    //
-    protocolVersion(Integer .class),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    @Deprecated
-    loggerLevel(String .class),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    @Deprecated
-    loggerFile(String .class),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    allowEncodingChanges(Boolean .class),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    logUnclosedConnections(Boolean .class),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    autosave(Enums.AutoSave .class, "never"),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    cleanupSavepoints(Boolean .class, "false"),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
-
-    binaryTransfer(Boolean .class, "true"),
-
-    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
     binaryTransferEnable(String .class),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     binaryTransferDisable(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
     databaseMetadataCacheFields(Integer .class, "65536"),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     databaseMetadataCacheFieldsMiB(Integer .class, "5"),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
     prepareThreshold(Integer .class),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     preparedStatementCacheQueries(Integer .class),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     preparedStatementCacheSizeMiB(Integer .class),
-    preferQueryMode(String.class),
 
-    defaultRowFetchSize(Integer.class),
-    loginTimeout(Integer.class),
-    connectTimeout(Integer.class),
-    socketTimeout(Integer.class),
 
-    cancelSignalTimeout(Integer.class),
-    tcpKeepAlive(Integer.class),
-    unknownLength(Integer.class),
-    stringtype(Integer.class),
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
-    ApplicationName(String.class),
-    kerberosServerName(Integer.class),
-    jaasApplicationName(Integer.class),
-    jaasLogin(Integer.class, "true"),
+    preferQueryMode(String .class),
 
-    gssEncMode(Enums.GSSEncMode.class, "DISABLE"),
-    gsslib(Integer.class),
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    defaultRowFetchSize(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    loginTimeout(Integer .class),
+
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    connectTimeout(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    socketTimeout(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    cancelSignalTimeout(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    tcpKeepAlive(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    unknownLength(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    stringtype(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    ApplicationName(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    kerberosServerName(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    jaasApplicationName(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    jaasLogin(Integer .class, "true"),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    gssEncMode(Enums.GSSEncMode .class, "DISABLE"),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    gsslib(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     // Since: 9.4
-    sspiServiceClass(Integer.class),
-    useSpnego(Boolean.class, "false"),
+    sspiServiceClass(Integer .class),
 
-    sendBufferSize(Integer.class),
-    receiveBufferSize(Integer.class),
-    readOnly(Integer.class),
-    readOnlyMode(Integer.class),
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
-    disableColumnSanitiser(Integer.class),
-    assumeMinServerVersion(String.class),
-    currentSchema(Integer.class),
-    targetServerType(Integer.class),
+    useSpnego(Boolean .class, "false"),
 
-    hostRecheckSeconds(Integer.class),
-    loadBalanceHosts(Integer.class),
-    socketFactory(Integer.class),
-    socketFactoryArg(Integer.class),
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
-    reWriteBatchedInserts(Integer.class),
-    replication(Integer.class),
-    escapeSyntaxCallMode(Integer.class),
-    maxResultBuffer(Integer.class),
-    hideUnprivilegedObjects(String.class),
-    logServerErrorDetail(String.class),
+    sendBufferSize(Integer .class),
 
-    PGDBNAME("PGDBNAME", String.class, null),
-    PGHOST("PGHOST", String.class, null),
-    PGPORT("PGPORT", String.class, null),
-    xmlFactoryFactory(String.class),
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
-    keyStoreType(String.class),
-    keyStoreUrl(String.class),
-    keyStorePassword(String.class),
+    receiveBufferSize(Integer .class),
 
-    trustStoreType(String.class),
-    trustStoreUrl(String.class),
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    readOnly(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    readOnlyMode(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    disableColumnSanitiser(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    assumeMinServerVersion(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    currentSchema(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    targetServerType(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    hostRecheckSeconds(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    loadBalanceHosts(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    socketFactory(Integer .class),
+
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    socketFactoryArg(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    reWriteBatchedInserts(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    replication(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    escapeSyntaxCallMode(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    maxResultBuffer(Integer .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    hideUnprivilegedObjects(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    logServerErrorDetail(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+
+    xmlFactoryFactory(String .class),
+
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    keyStoreType(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    keyStoreUrl(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    keyStorePassword(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    trustStoreType(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
+    trustStoreUrl(String .class),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
     trustStorePassword(String .class),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     fallbackToSystemKeyStore(Boolean .class, "false"),
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
     factoryWorkerCount(Integer .class, "100"),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     hexEscapes(Boolean .class, "false"),
 
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
+
     lc_monetary(String .class);
+
+    public static final PgKey<String> k = new PgKey<>("", String.class, null);
 
 
     private static final List<PgKey<?>> KEY_LIST = createKeyList();
